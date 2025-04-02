d80ac75fad9c8b86cbd20f74becef23647cc668b
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-04-02 14:01:40 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const goodbye = (jmeno, prijmeni) => {
+    return `Na shledanou ${jmeno} ${prijmeni}`
+};
 
+const emailClosingElement = document.querySelector('.email__closing')
+emailClosingElement.textContent = goodbye('Pavel', 'Ovesný');
+
+
+const fillSubject = (subject) => {
+    const subjectElement = document.querySelector('.email__subject')
+    subjectElement.textContent = subject
+    return subjectElement;
+}
+fillSubject('Hledám práci');
+
+const fillBody = (body, name) => {
+    const bodyElement = document.querySelector('.email__body')
+    bodyElement.textContent = body
+    const goodbye = document.querySelector('.email__closing')
+    goodbye.textContent = name
+    return {bodyElement, goodbye} ;
+}
+fillBody ('Líbí se mi váš inzerát', 'Eliška Ovesná');
